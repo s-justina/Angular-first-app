74f4f210844589c09483a0999482f65200b9f1ad
Justyna <jus.sobcz@gmail.com>
2024-01-29 09:55:45 +0100

    feat: add basic routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,17 @@
-import {Component, Input} from '@angular/core';
-import {RouterOutlet} from '@angular/router';
+import {Component} from '@angular/core';
+import {RouterLink, RouterOutlet} from '@angular/router';
 import {CommonModule} from "@angular/common";
-import {InfoComponent} from "./info/info.component";
-import {FormsModule} from "@angular/forms";
-import {HobbyElementComponent} from "./hobbyElement/hobbyElement.component";
+import {HobbiesComponent} from "./hobbies/hobbies.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, InfoComponent, FormsModule, HobbyElementComponent],
+  imports: [RouterOutlet, CommonModule, HobbiesComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
 
 export class AppComponent {
-  hobbies: string[] = ["sport", "reading books"];
-  @Input() newHobby: string;
-  hobbyDeleted: boolean = false;
 
-  addNewHobby() {
-    this.hobbies.push(this.newHobby);
-  };
-
-  onHobbyRemoved(element: string) {
-    const position = this.hobbies.indexOf(element);
-    this.hobbies.splice(position, 1);
-    this.hobbyDeleted = true;
-
-    setTimeout(() => {
-      this.hobbyDeleted = false;
-    }, 3000)
-  }
 }
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import {HobbiesComponent} from "./hobbies/hobbies.component";
+import {BlogComponent} from "./blog/blog.component";
+import {AccountComponent} from "./account/account.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{
+  path:"",
+  component: HobbiesComponent
+},{
+  path: "blog",
+  component:BlogComponent
+},{
+  path: "account",
+  component: AccountComponent
+}];
